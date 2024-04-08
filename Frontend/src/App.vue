432fb7b17bd639b735f235da88f5004ee1a8ea37
Léandro Benites <leandro.benites@focusent.com>
2024-04-08 10:38:58 +0200

    feat(Tower): Sort start
    Started the sorting buttons

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,4 +1,18 @@
 <script lang="ts">
+enum SORT_ORDER {
+  NAME,
+  STATUS,
+  TAG,
+  NONE,
+}
+enum SHOW_ONLY {
+  DOWN,
+  UP,
+  BROKEN,
+  ALL,
+}
+let sort = ref(SORT_ORDER.NONE)
+let display = ref(SHOW_ONLY.ALL);
 let props:{projectName:string,code:number}[]
   try{
   const res = await fetch("http://localhost:8080/requestStatus");
@@ -7,8 +21,33 @@ let props:{projectName:string,code:number}[]
   catch(err){
     props = [{projectName:"Entertainment",code:404},{projectName:"Frei",code:200},{projectName:"ShadowBound",code:150},{projectName:"Vi",code:200},{projectName:"Bebou",code:404},{projectName:"Floriane",code:300},{projectName:"Leandro",code:404},{projectName:"Mario",code:200},{projectName:"Springles",code:100}]
 }
-const show = ref(props)
-
+let show = ref(props)
+function changeState(_sort?:SORT_ORDER, _display?:SHOW_ONLY){
+//#region show only
+    if(_display == SHOW_ONLY.ALL || display.value == _display)
+      show.value = props;
+    if(_display == SHOW_ONLY.BROKEN){
+      show.value = props.filter(x => x.code >200 && x.code != 404)
+    }
+    if(_display == SHOW_ONLY.DOWN){
+      show.value = props.filter(x => x.code == 404)
+    }
+    if(_display == SHOW_ONLY.UP){
+      show.value = props.filter(x => x.code == 200)
+    }
+  //#endregion
+  if(_sort != sort.value){
+    if(_sort == SORT_ORDER.NAME){
+    show.value = show.value.sort((a, b) => a.projectName > b.projectName ? 1 : -1)
+  }
+  if(_sort == SORT_ORDER.STATUS){
+    show.value = show.value.sort((a, b) => a.code - b.code)
+  }
+  if(_sort == SORT_ORDER.TAG){
+    throw new Error("not Implemented")
+  }
+  }
+}
 </script>
 
 <script setup lang="ts">
@@ -30,14 +69,15 @@ import card from './components/card.vue';
     <div id="body-content">
       <div id="side-bar">
         <p class="sortCategory">Sort By</p>
-        <button class="sortButton" @click="show = show.sort((a, b) => a.projectName > b.projectName ? 1 : -1)">Name</button>
-        <button class="sortButton" @click="show = show.sort((a, b) => a.code - b.code)">Status</button>
-        <button class="sortButton">Tag</button>
+        <button class="sortButton" @click="changeState(SORT_ORDER.NAME)">Name</button>
+        <button class="sortButton" @click="changeState(SORT_ORDER.STATUS)">Status</button>
+        <button class="sortButton" @click="changeState(SORT_ORDER.TAG)">Tag</button>
         <p class="sortCategory">Show Only</p>
-        <button class="sortButton" @click="show = props.filter(x => x.code == 404)">Down</button>
-        <button class="sortButton" @click="show = props.filter(x => x.code == 200)">Up</button>
-        <button class="sortButton" @click="show = props.filter(x => x.code >200 && x.code != 404)">Broken</button>
-
+        <button class="sortButton" @click="changeState(SHOW_ONLY.DOWN)">Down</button>
+        <button class="sortButton" @click="">Up</button>
+        <button class="sortButton" @click="">Broken</button>
+        
+        
       </div>
       <div id="app">
         <p> {{show}}</p>    
